eda1e0819e7cd955ce3207038d0706b6c089d1e4
ashimusmani <ashimusmani@gmail.com>
2021-05-21 14:17:26 +0530

    Allow project names with dashes and underscores, Fixed #2.

--- src/main/java/org/zigmoi/ketchup/common/validations/ValidProjectId.java
+++ src/main/java/org/zigmoi/ketchup/common/validations/ValidProjectId.java
@@ -17,7 +17,7 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 @NotBlank(message = "Project name (projectResourceId) should be minimum 2 and maximum 20 in length.")
 @Size(min= 2, max = 20, message = "Project name (projectResourceId) should be minimum 2 and maximum 20 in length.")
-@Pattern(regexp = "[a-zA-Z0-9]*", message = "Project name (projectResourceId) should only contain alphabets and numbers.")
+@Pattern(regexp = "[a-zA-Z0-9-_]*", message = "Project name (projectResourceId) should only contain alphabets, numbers, dashes and underscores.")
 @Target({ TYPE, METHOD, FIELD, PARAMETER, ANNOTATION_TYPE })
 @Retention(RUNTIME)
 @Constraint(validatedBy = { })
